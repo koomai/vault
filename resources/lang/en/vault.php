4a53fe5d51168ecaf0877cf27b35ae243d1bdc28
Sid <sid@koomai.net>
2015-06-10 17:09:49 +1000

    organised language file

--- resources/lang/en/vault.php
+++ resources/lang/en/vault.php
@@ -1,32 +1,58 @@
 <?php
 
-return array(
+/*
+|--------------------------------------------------------------------------
+| Vault Language File
+|--------------------------------------------------------------------------
+|
+| This file contains all the copy used in the app. You can use it to easily
+| customise the copy in another language or simply change some text.
+|
+*/
 
-    'heading' 				=> 'Vault',
-    'intro' 				=> 'Sensitive information sent via email lives forever. Not with Vault.',
-    'intro2' 				=> 'Great for sharing login details and passwords with coworkers and clients.',
-    'great'     			=> 'Don\'t be a n00b. Use Vault to send passwords and logins securely.',
-    'text' 					=> 'Text to Encrypt and Expire',
-    'text_info' 			=> 'Encrypted in your browser, not on the server.',
-    'shared_password'     	=> 'Password',
-    'shared_password_info'	=> 'A password is required to decrypt the text',
-    'expire_after'     		=> 'Text Expires After',
-    'minutes'				=> ':minutes Minutes',
-    'hour'     				=> ':hour Hour',
-    'hours'     			=> ':hours Hours',
-    'days'     				=> ':days Days',
-    'pt1'     				=> 'Sending login details to support',
-    'pt2'     				=> 'Passing secret URLs to coworkers',
-    'pt3'     				=> 'Sharing passwords with coworkers',
-    'footer'				=> 'Built with Lumen and Semantic UI',
+return [
+
+    'copy'      => [
+            'heading'       => 'Vault',
+            'sub_heading'   => 'Share sensitive information securely',
+            'footer'		=> 'Built with <a href="http://lumen.laravel.com/">Lumen</a> and <a href="http://semantic-ui.com/">Semantic UI</a>.',
+    ],
+
+    'sidebar'   =>  [
+        'intro' 		    => 'Sensitive information sent via email lives forever. Not with Vault.',
+        'intro2' 		    => 'Great for sharing login details and passwords with coworkers and clients.',
+        'intro3'     	    => 'Don\'t be a n00b. Use Vault to send passwords and logins securely.',
+    ],
+
+    'form'      => [
+        'text_label'        => 'Text to Encrpyt',
+        'text_placeholder'  => 'Enter text here',
+        'password_label'    => 'Password',
+        'password_placeholder'  => 'Password to decrypt text',
+        'expire_label'      => 'Text Expires After',
+        'minutes'			=> ':minutes Minutes',
+        'hour'     			=> ':hour Hour',
+        'hours'     		=> ':hours Hours',
+        'days'     			=> ':days Days',
+        'submit_button'		=> 'Create Private Encrypted Link',
+
+        'decrypt_password_label'        => 'Password',
+        'decrypt_password_placeholder'	=> 'Enter the password you received with the link',
+        'secret_text_label'             => 'Secret Text',
+        'decrypt_button'                => 'Display Message',
+    ],
+
+    'error_title'           => 'Oops! Something went wrong.',
+    'error_text'            => 'There was an error encrypting your text. Please try again.',
+    'incorrect_password_title'  => 'Incorrect Password',
+    'incorrect_password_text'   => 'Please try again with the correct password.',
+
+    'loader_text'           => 'Decrypting',
     'copy_to_clipboard'		=> 'Copy URL to Clipboard & Close',
     'modal_title'			=> 'Your text has been successfully encrypted',
-    'modal_message'			=> 'Share your encrypted message using the link and password below.',
-    'decrypt_password'		=> 'Text Password',
-    'decrypt_password_info'	=> 'You should have received this with the link.',
-    'message'				=> 'Secret Text',
-    'display_message'		=> 'Display Message',
-    'copymessage'			=> "Temporary Encrypted Link\n------------------------------------------\nURL:         %link%\nPassword: %password%\n------------------------------------------\n",
-    'expired_link'			=> 'This link has expired. The contents of the text has been deleted and is no longer available.',
-    'submit'				=> 'Create Private Encrypted Link'
-);
+    'modal_text'			=> 'Share your encrypted message using the link and password below.',
+    'modal_message'			=> "Temporary Encrypted Link\n------------------------------------------\nURL:         %link%\nPassword: %password%\n------------------------------------------\n",
+
+    'expired_link_title'		=> 'This link has expired.',
+    'expired_link_text'			=> 'The contents of this link has been deleted and is no longer available.',
+];
