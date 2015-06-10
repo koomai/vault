49226edc211c9a2b2fa03cb928ed19ff26cea858
Sid <sid@koomai.net>
2015-06-10 17:08:43 +1000

    added 30 mins to expiry options

--- config/vault.php
+++ config/vault.php
@@ -12,13 +12,14 @@ return [
     */
 
     'minutes' => [
-        '60'     => trans('vault.hour', ['hour' => 1]),
-        '720'    => trans('vault.hours', ['hours' => 12]),
-        '1440'   => trans('vault.hours', ['hours' => 24]),
-        '2880'   => trans('vault.hours', ['hours' => 48]),
-        '10080'  => trans('vault.days', ['days' => 7]),
-        '20160'  => trans('vault.days', ['days' => 14]),
-        '43200'  => trans('vault.days', ['days' => 30]),
+        '30'     => trans('vault.form.minutes', ['minutes' => 30]),
+        '60'     => trans('vault.form.hour', ['hour' => 1]),
+        '720'    => trans('vault.form.hours', ['hours' => 12]),
+        '1440'   => trans('vault.form.hours', ['hours' => 24]),
+        '2880'   => trans('vault.form.hours', ['hours' => 48]),
+        '10080'  => trans('vault.form.days', ['days' => 7]),
+        '20160'  => trans('vault.form.days', ['days' => 14]),
+        '43200'  => trans('vault.form.days', ['days' => 30]),
     ],
 
     /*
@@ -26,10 +27,9 @@ return [
     | Max text size (in bytes)
     |--------------------------------------------------------------------------
     |
-    | Generally PasteVault is designed to store small bits of text so don't allow
-    | text larger than this. Note, this limit is checked after the text has been
-    | encrypted and base 64 encoded client side which means is't significantly
-    | larger than the original text entered by the user.
+    | Don't allow text larger than this. Note, this limit is checked after the text
+    | has been encrypted and base 64 encoded client side which means isn't
+    | significantly larger than the original text entered by the user.
     |
     */
 
@@ -40,24 +40,8 @@ return [
     | Google Analytics ID
     |--------------------------------------------------------------------------
     |
-    | If you enter your analytics ID the GA code will be added to the site.
-    |
-    | Below is currently setup for PHPFog, but a regular string key can be used
-    | in most situations.
+    | Enter your Google Analytics ID in "UA-XXXXXX" format for tracking
     */
 
     'google_analytics' => false,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Terms of service company
-    |--------------------------------------------------------------------------
-    |
-    | The company running this hosted instance of PasteVault
-    |
-    | Below is currently setup for PHPFog, but a regular string key can be used
-    | in most situations.
-    */
-
-    'company' => "Blackglass",
 ];
